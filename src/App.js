b2995fcacebf531a7329af8461916873f7b14a70
wbdvlpmnt <jai.gokhale5219@gmail.com>
2023-12-19 13:03:36 -0600

    data fetching

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
 // `https://api.frankfurter.app/latest?amount=100&from=EUR&to=USD`
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 export default function App() {
   const [amount, setAmount] = useState(0);
+  const [convertedAmount, setConvertedAmount] = useState(0);
+
+  useEffect(
+    function () {
+      const controller = new AbortController();
+
+      async function fetchData() {
+        try {
+          const res = await fetch(
+            `https://api.frankfurter.app/latest?amount=${amount}&from=EUR&to=USD`,
+            { signal: controller.signal }
+          );
+          const data = await res.json();
+          setConvertedAmount(data.rates[Object.keys(data.rates)[0]]);
+        } catch (err) {
+          console.log(err.message);
+        }
+      }
+
+      fetchData();
+    },
+    [amount]
+  );
 
   return (
     <div>
@@ -24,7 +47,7 @@ export default function App() {
         <option value="CAD">CAD</option>
         <option value="INR">INR</option>
       </select>
-      <p>OUTPUT</p>
+      <p>{convertedAmount}</p>
     </div>
   );
 }
