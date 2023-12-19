826e93ab7579736e24d9b5ff6246144ea3a130a8
wbdvlpmnt <jai.gokhale5219@gmail.com>
2023-12-19 11:11:05 -0600

    create controlled component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 // `https://api.frankfurter.app/latest?amount=100&from=EUR&to=USD`
+import { useState } from "react";
 import "./App.css";
 
 export default function App() {
+  const [amount, setAmount] = useState(0);
+
   return (
     <div>
-      <input type="text" />
+      <input
+        type="text"
+        value={amount}
+        onChange={(e) => setAmount(e.target.value)}
+      />
       <select>
         <option value="USD">USD</option>
         <option value="EUR">EUR</option>
